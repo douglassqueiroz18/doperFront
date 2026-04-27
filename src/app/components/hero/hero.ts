8800fdf2d233cb25a69a16981cf97cf498816232
douglassqueiroz18 <douglas.queiroz25@outlook.com>
2026-04-26 22:06:32 -0400

    Subindo pequena modificação de contato no whatsapp

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -4,8 +4,17 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-hero',
+  standalone: true,
   imports: [MatButtonModule, MatIconModule],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  sendWhatsAppMessage() {
+    const phoneNumber = '5567998012295';
+    const message = encodeURIComponent('Olá! Gostaria de saber mais sobre os serviços da Doper.');
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
+    
+    window.open(url, '_blank');
+  }
+}
